66c176ddc81fb69b2b9faa44952062836f293a8e
Rajitha Fernando <chandimalfdo23@gmail.com>
2022-10-20 14:43:40 +0530

    removed unwanted code

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -6,7 +6,7 @@
     @include('docgenviews::meta', [
         'title' => $title,
         'image' => '',
-        'canonical' => "https://blogs.hyvor.com/docs/$pageName",
+        // 'canonical' => "https://blogs.hyvor.com/docs/$pageName",
     ])
 </head>
 <body class="docs-page {{$pageName}}">
@@ -37,9 +37,13 @@
 
     </div>
 
-
     <div id="content-view">
 
+        {{-- <button id="docs-mobile" onclick="handleMobile()">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+            </svg>
+        </button> --}}
 
         <content>
             {!! $content !!}
@@ -48,11 +52,36 @@
     </div>
 
     <script>
+
+        // mobile
+        // function handleMobile() {
+        //     var btn = document.getElementById("docs-mobile");
+        //     var sidebar = document.getElementById("sidebar");
+
+        //     var rect = btn.getBoundingClientRect()
+        //     sidebar.style.left = rect.left + "px";
+        //     sidebar.style.top = rect.top + 40 + "px";
+        //     sidebar.classList.toggle('open');
+        // }
+
+
         // nav pos on scroll
-        window.addEventListener('scroll', function() {
+
+        function selfRemovingWindowEvent(on, func) {
+            var remover = function() {
+                window.removeEventListener(on, func);
+                window.removeEventListener('flashload:navigationStarted', remover)
+            }
+            window.addEventListener(on, func);
+            window.addEventListener('flashload:navigationStarted', remover)
+        }
+
+        function handleScroll() {
+            if (window.innerWidth <= 992) return;
             var nav = document.getElementById("sidebar");
             nav.style.top = Math.max(65-window.scrollY, 15) + "px";
-        });
+        }
+        selfRemovingWindowEvent('scroll', handleScroll)
 
         // scroll active
         var active = document.querySelector(".nav-page.active");
@@ -90,7 +119,7 @@
         }
     </script>
 
-    <script src="docgenviews::/../js-static/docs-prism.js"></script>
+    <script src="{{ asset('docgenpackage/js/docs-prism.js')}}"></script>
 
 </body>
 </html>
--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,5 +1,4 @@
 <nav>
-
 	<div class="container">
 		<a class="nav-brand" href="/">
 			<img src="<?= config('blogs.logo') ?>" alt="Hyvor Blogs LOGO" width="30" height="30" />
@@ -8,12 +7,33 @@
 
 		<div class="navbar-content">
 			<div class="navbar-nav">
-				<a class="nav-item nav-link{{ request()->is('themes*') ? " active" : ""  }}" href="/themes">Themes</a>
 				<a class="nav-item nav-link{{ request()->is('docs*') ? " active" : ""  }}" href="/docs">Docs</a>
-				<a class="nav-item nav-link{{ request()->is('pricing') ? " active" : ""  }}" href="/pricing">Pricing</a>
-				<a class="nav-item nav-link" href="/console" data-flashload-skip>Console</a>
 			</div>
+			<button id="nav-mobile" onclick="toggleNavBar(event)">
+				<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+					<path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+				</svg>
+			</button>
 		</div>
 	</div>
+
+	<script>
+		function toggleNavBar(e) {
+			var navbar = document.querySelector('.navbar-nav');
+			e && e.stopPropagation();
+			if (navbar.classList.contains('open')) {
+				navbar.classList.remove('open');
+				window.removeEventListener('click', handleNavBarOut);
+			} else {
+				navbar.classList.add('open')
+				window.addEventListener('click', handleNavBarOut)
+			}
+		}
+		function handleNavBarOut(e) {
+			if (!document.querySelector('.navbar-nav').contains(e.target)) {
+				toggleNavBar();
+			}
+		}
+	</script>
 
 </nav>
