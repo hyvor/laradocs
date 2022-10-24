8d8e844f68cd2c9e3176e9cb918c46117bdd4103
Rajitha Fernando <chandimalfdo23@gmail.com>
2022-10-24 10:38:32 +0530

    change header design

--- config/config.php
+++ config/config.php
@@ -1,13 +1,14 @@
 <?php
 
 return [
-    'logo_url' => 'https://blogs.hyvor.com/img/logo.png', //logo image url
+    'app_url' => 'documents', //your app url
+    'content_files_path' => 'docs/pages', //md files path
     'brand_name' => 'My Blog', //your blog name
-    'main_link' => '', //redirect link when click on logo
+    'logo_url' => 'https://blogs.hyvor.com/img/logo.png', //logo image url
+    'main_link' => '/documents', //redirect link when click on logo
    
     'theme' => 'theme', //theme css file name
-    'app_url' => 'test', //your app url
-    'content_files_path' => 'docs/pages', //md files path
+
     'nav' => [
         'Intro' => [
             [null, 'Getting Started'],
