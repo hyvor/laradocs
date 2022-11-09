270df62c1d8f17e854eb33af4bcbaa1e5fe5ffeb
Rajitha Fernando <chandimalfdo23@gmail.com>
2022-11-09 13:44:32 +0530

    cache test done

---
+++ tests/Unit/CacheCommandTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use Hyvor\Laradocs\Facades\ContentProcessor;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Cache;
+
+beforeEach(function(){
+    $this->config = config('laradocs_config');
+});
+
+test('check if cache set for all config links',  function() {
+    Artisan::call('laradocs:cache');
+   
+    foreach($this->config as $doc){
+        foreach($doc['navigation'] as $page){
+            foreach($page as $link){
+                $pageLink = !empty($link['id']) ? $link['id'] : 'index';
+                $cacheKey = $doc['route'].'|'.$pageLink;
+
+                $response = ContentProcessor::cacheViews($doc['route'], $link['id']);
+                $this->assertEquals(Cache::store('file')->get($cacheKey), $response);
+            }
+        }
+    }
+});
